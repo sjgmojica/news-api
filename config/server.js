3d2af9faca37b64f06ddaa024c4b575af28fd6e4
sjgmojica <mojica@mclinica.com>
2019-11-12 08:51:47 +0800

    add cors package and use it in server.js

--- config/server.js
+++ config/server.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 import express from "express";
 import bodyParser from "body-parser";
+import cors from 'cors';
+
 const server = express();
 
+server.use(cors());
 server.use(bodyParser.json());
 
 import setRoutes from './routes';
